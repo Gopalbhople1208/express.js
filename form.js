6b4677e7fcb9dfaac0d0505450cb005f9d98b161
Gopalbhople <gbhople63@gmail.com>
2026-02-21 18:27:50 +0530

    ths use of ejs help to how to render the page of user screen

--- form.js
+++ form.js
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.get()
+app.use(express.urlencoded({extended:false}))
+
+
+app.set('view engine','ejs')
+app.get("/add-user",(req,resp)=>{
+    resp.render('addUser');
+})
+app.post("/submit-user",(req,resp)=>{
+   
+resp.render('submitUser',req.body);
+})
+app.listen(2020); //this is userName: Gopal_coder ,this is userEmail: gopalarun64@gmail.com ,this is userAge: 19
