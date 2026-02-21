01f989cc1d88204cea9b4bdadbb63e0d268ee31d
Gopalbhople <gbhople63@gmail.com>
2026-02-21 19:07:29 +0530

    this conditional loop use to ejs

--- loop.js
+++ loop.js
@@ -1,18 +1,17 @@
 import express from 'express';
 
 const app = express();
-
 app.use(express.urlencoded({extended:false}))
-
-
 app.set('view engine','ejs')
 app.get("/add-user",(req,resp)=>{
     resp.render('addUser');
 })
 app.post("/submit-user",(req,resp)=>{
-   
-resp.render('submitUser',req.body);
+    resp.render('submitUser',req.body);
+})
+app.get("/users",(req,resp)=>{
+    const users =['anil','sunil','gopal','krishna','rahul','sahil','gau']
+    resp.render('users',{users:users,isLogin:false});
 })
 
-
-app.listen(2020); //this is userName: Gopal_coder ,this is userEmail: gopalarun64@gmail.com ,this is userAge: 19
+app.listen(3000);
